c8f08fd71859940aaddd4325f4d81d4c0aa88b85
Ben <benzuvsteel@gmail.com>
2018-02-18 16:35:12 -0800

    extracted store to configureStore

--- src/app/store.js
+++ src/app/store.js
@@ -7,35 +7,38 @@ import createHistory from 'history/createBrowserHistory';
 import rootReducer from './root-reducer';
 
 export const history = createHistory();
-const sagaMiddleware = createSagaMiddleware();
 
-const initialState = {};
-const enhancers = [];
-const middleware = [
-  routerMiddleware(history),
-  sagaMiddleware,
-  logger
-];
+const configureStore = () => {
+  const initialState = {};
+  const enhancers = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middleware = [
+    routerMiddleware(history),
+    sagaMiddleware,
+    logger
+  ];
 
-if (process.env.NODE_ENV === 'development') {
-  const devToolsExtension = window.devToolsExtension
+  if (process.env.NODE_ENV === 'development') {
+    const devToolsExtension = window.devToolsExtension
 
-  if (typeof devToolsExtension === 'function') {
-    enhancers.push(devToolsExtension())
+    if (typeof devToolsExtension === 'function') {
+      enhancers.push(devToolsExtension())
+    }
   }
+
+  const composedEnhancers = compose(
+    applyMiddleware(...middleware),
+    ...enhancers
+  );
+
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composedEnhancers
+  );
+
+  sagaMiddleware.run(rootSaga);
+  return store;
 }
 
-const composedEnhancers = compose(
-  applyMiddleware(...middleware),
-  ...enhancers
-);
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  composedEnhancers
-);
-
-sagaMiddleware.run(rootSaga);
-
-export default store;
+export default configureStore;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import store, { history } from './app/store';
+import configureStore, { history } from './app/store';
 import App from './app/components/App';
 
 import './app/style/App.css';
 import './index.css';
 
 const target = document.querySelector('#root')
+const store = configureStore();
 
 render(
   <Provider store={store}>
