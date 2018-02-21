f943d58b5d611074d09df74f251156eef379b25b
Ben <benzuvsteel@gmail.com>
2018-02-20 23:41:48 -0800

    changed api calls to template literals

--- src/home/api-calls.js
+++ src/home/api-calls.js
@@ -1,11 +1,10 @@
 import { api_key } from '../api-keys';
 
 // return list of all videos by publish date in descending order
-export const requestHomeVideos = page => (
-  "https://www.giantbomb.com/api/videos/" +
-  "?api_key=" + api_key +
-  "&format=jsonp&json_callback=callback&limit=24&offset=" +
-  (page-1) * 24 +
-  "&field_list=hd_url,name,id,deck,embed_player,publish_date,image,video_type," +
-  "user&filter=video_type:3|8|23&sort=publish_date:desc"
+export const requestHomeVideos = page => ([
+  `https://www.giantbomb.com/api/videos/?api_key=${api_key}`,
+  `&format=jsonp&json_callback=callback&limit=24&offset=${(page-1) * 24}`,
+  `&field_list=hd_url,name,id,deck,embed_player,publish_date,image,video_type,user`,
+  `&filter=video_type:3|8|23&sort=publish_date:desc`
+  ].join('')
 )
--- src/quicklooks/api-calls.js
+++ src/quicklooks/api-calls.js
@@ -1,11 +1,10 @@
 import { api_key } from '../api-keys';
 
 // return list of all quicklook videos by publish date in descending order
-export const requestQuicklookVideos = page => (
-  "https://www.giantbomb.com/api/videos/" +
-  "?api_key=" + api_key +
-  "&format=jsonp&json_callback=callback&limit=24&offset=" +
-  (page-1) * 24 +
-  "&field_list=hd_url,name,id,deck,embed_player,publish_date,image,video_type," +
-  "user&filter=video_type:3&sort=publish_date:desc"
+export const requestQuicklookVideos = page => ([
+  `https://www.giantbomb.com/api/videos/?api_key=${api_key}`,
+  `&format=jsonp&json_callback=callback&limit=24&offset=${(page-1) * 24}`,
+  `&field_list=hd_url,name,id,deck,embed_player,publish_date,image,video_type,user`,
+  `&filter=video_type:3&sort=publish_date:desc`
+  ].join('')
 )
