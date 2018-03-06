e733ab5cd24112ffc920b65f76cf9fdb653c4aba
Ben <benzuvsteel@gmail.com>
2018-03-05 23:32:52 -0800

    clicking on search result option now populates field with video name and retains search results

--- src/containers/search-bar.jsx
+++ src/containers/search-bar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
-import { Select, Spin } from 'antd';
+import { Select } from 'antd';
 import _ from 'lodash';
 import { setSearchSpinner } from '../actions/index';
 
@@ -18,8 +18,7 @@ class SearchBar extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
-    this.handleSearch = _.debounce(this.handleSearch, 500);
-    this.handleSelect = this.handleSelect.bind(this);
+    this.handleSearch = _.debounce(this.handleSearch, 450);
   }
 
   handleChange(value) {
@@ -34,34 +33,43 @@ class SearchBar extends Component {
     this.props.fetchVideos(value);
   }
 
-  handleSelect(value) {
-    this.props.setVideo(value);
-  }
-
   render() {
     const results = this.props.currentResults.map(result => (
-      <Option key={result.id} style={{ lineHeight: '22px' }}>
-        <img
-          src={result.image.icon_url}
-          alt=""
-          style={{
-            display: 'inline-block',
-            lineHeight: 'normal',
-            verticalAlign: 'middle',
-            height: '32px',
-            width: '32px',
-          }}
-        />
+      <Option
+        key={result.name}
+        style={{ lineHeight: '22px' }}
+      >
         <div
-          style={{
-            display: 'inline-block',
-            lineheight: 'normal',
-            verticalAlign: 'middle',
-            marginLeft: '5px',
+          onClick={() => this.props.setVideo(result.id)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              this.props.setVideo(result.id);
+            }
           }}
         >
-          <div>{result.name}</div>
-          <div>Video published {result.publish_date}</div>
+
+          <img
+            src={result.image.icon_url}
+            alt=""
+            style={{
+              display: 'inline-block',
+              lineHeight: 'normal',
+              verticalAlign: 'middle',
+              height: '32px',
+              width: '32px',
+            }}
+          />
+          <div
+            style={{
+              display: 'inline-block',
+              lineheight: 'normal',
+              verticalAlign: 'middle',
+              marginLeft: '5px',
+            }}
+          >
+            <div>{result.name}</div>
+            <div>Video published {result.publish_date}</div>
+          </div>
         </div>
       </Option>
     ));
@@ -74,10 +82,11 @@ class SearchBar extends Component {
         placeholder={this.props.placeholder}
         defaultActiveFirstOption={false}
         filterOption={false}
-        notFoundContent={this.props.searchSpinnerOn ? <Spin size="small" /> : null}
+        notFoundContent={
+          this.state.query !== '' ? 'we got nothin\' for you bruh' : null
+        }
         onChange={this.handleChange}
         onSearch={this.handleSearch}
-        onSelect={this.handleSelect}
       >
         {results}
       </Select>
@@ -91,7 +100,6 @@ SearchBar.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   currentResults: PropTypes.array.isRequired,
   setSearchSpinner: PropTypes.func.isRequired,
-  searchSpinnerOn: PropTypes.bool.isRequired,
   setVideo: PropTypes.func.isRequired,
 };
 
